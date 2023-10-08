b9cbf821d4b69d8242ceda9ebc3a4d86f9a3d5be
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-10-08 18:13:21 +0300

    start.py

--- start.py
+++ start.py
@@ -16,12 +16,12 @@ from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 def setup_platform(hass, config, add_devices.discovery_info=None):
   add_devices([ExampleSensor()])
 class ExampleSensor(Entity):
-  @property
+  
   def name(self):
     return 'Temperature'
-  @property
+  
   def state(self):
     return 23
-  @property
+      
   def unit_of_measurement(self):
     return TEMP_CELSIUS
