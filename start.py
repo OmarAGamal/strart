8eaf677b0564188e7e3a10689774996bbcbec82c
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-10-08 17:37:18 +0300

    start.py

---
+++ start.py
@@ -0,0 +1,27 @@
+"""Platform for start integration."""
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.helpers.entity import Entity
+from homeassistant.const import UnitOfTemperature
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+def setup_platform(hass, config, add_devices.discovery_info=None):
+  add_devices([ExampleSensor()])
+class ExampleSensor(Entity):
+  @property
+  def name(self):
+    return 'Temperature'
+  @property
+  def state(self):
+    return 23
+  @property
+  def unit_of_measurement(self):
+    return TEMP_CELSIUS
